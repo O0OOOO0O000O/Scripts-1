bf3fb048407653db99e9523aef24027f437512fc
Emcept <83399555+Emcept@users.noreply.github.com>
2022-08-03 20:49:36 +0200

    Update test4.lua

--- test4.lua
+++ test4.lua
@@ -2,21 +2,7 @@ if not game:IsLoaded() then
 	game.Loaded:Wait()
 	end
 
-function tp()
-	local x = {}
 	
-	pcall(function()
-			for _, v in ipairs(game:GetService("HttpService"):JSONDecode(game:HttpGetAsync("https://games.roblox.com/v1/games/" .. game.PlaceId .. "/servers/Public?sortOrder=Asc&limit=100")).data) do
-				if type(v) == "table" and v.maxPlayers > v.playing and v.id ~= game.JobId then
-					x[#x + 1] = v.id
-				end
-			end
-			if #x > 0 then
-				game:GetService("TeleportService"):TeleportToPlaceInstance(game.PlaceId, x[math.random(1, #x)])
-			end
-	end)
-end
-
 
 
 if game.PlaceVersion ~= 12 and game.PlaceVersion ~= 11 then
@@ -30,7 +16,18 @@ if game.PlaceVersion ~= 12 and game.PlaceVersion ~= 11 then
 	end)
     
     for i = 1, 100 do task.wait(.2)
-		tp()
+		local x = {}
+	
+	pcall(function()
+			for _, v in ipairs(game:GetService("HttpService"):JSONDecode(game:HttpGetAsync("https://games.roblox.com/v1/games/" .. game.PlaceId .. "/servers/Public?sortOrder=Asc&limit=100")).data) do
+				if type(v) == "table" and v.maxPlayers > v.playing and v.id ~= game.JobId then
+					x[#x + 1] = v.id
+				end
+			end
+			if #x > 0 then
+				game:GetService("TeleportService"):TeleportToPlaceInstance(game.PlaceId, x[math.random(1, #x)])
+			end
+	end)
 		end
     
 end
