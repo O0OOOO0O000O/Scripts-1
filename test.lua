43166c04cacb5ea67e85f469936aec2087be8e4a
Emcept <83399555+Emcept@users.noreply.github.com>
2022-08-03 20:24:46 +0200

    Create test.lua

---
+++ test.lua
@@ -0,0 +1,23 @@
+local ver = game.PlaceVersion
+
+if ver >=13 then
+    
+    local queueteleport = syn and syn.queue_on_teleport or queue_on_teleport or fluxus and fluxus.queue_on_teleport
+
+	game:GetService("Players").LocalPlayer.OnTeleport:Connect(function(State)
+		if State == Enum.TeleportState.Started then
+			queueteleport("loadstring(game:HttpGet'https://raw.githubusercontent.com/Emcept/Scripts/main/test.lua')()")
+		end
+	end)
+    
+    
+    local x = {}
+			for _, v in ipairs(game:GetService("HttpService"):JSONDecode(game:HttpGetAsync("https://games.roblox.com/v1/games/" .. game.PlaceId .. "/servers/Public?sortOrder=Asc&limit=100")).data) do
+				if type(v) == "table" and v.maxPlayers > v.playing and v.id ~= game.JobId then
+					x[#x + 1] = v.id
+				end
+			end
+			if #x > 0 then
+				game:GetService("TeleportService"):TeleportToPlaceInstance(game.PlaceId, x[math.random(1, #x)])
+			end
+end
